ab9c85bd31ad5c720052671e1e87f0acb0b7f0ee
91195608 <91195608@alunos.ipbeja.pt>
2013-03-19 18:24:22 +0000

    update files

--- src/appSegInformatica.py
+++ src/appSegInformatica.py
@@ -4,3 +4,17 @@ Created on 19 de Mar de 2013
 
 @author: admin1
 '''
+
+import sys
+import re
+import pygeoip
+gi = pygeoip.GeoIP('../../../GeoIP.dat', pygeoip.MEMORY_CACHE)
+print gi.country_code_by_name('www.baidu.com')
+for line in sys.stdin:
+    if not re.search("SRC=192", line):
+        lista = line.split("SRC=")
+    IP = lista[1].split(' ' )[0]
+    try:
+        print IP + ': ' + gi.country_code_by_addr(IP)
+    except:
+        continue
