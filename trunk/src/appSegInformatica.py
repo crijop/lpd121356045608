6c05f71bf03078c9265e8a8bc8520425d157ecce
91195608@alunos.ipbeja.pt <91195608@alunos.ipbeja.pt@7f4d7958-b159-f3d1-c01b-6b2bf8c276db>
2013-04-10 15:59:39 +0000

    Alteração da análise dos file logs

--- trunk/src/appSegInformatica.py
+++ trunk/src/appSegInformatica.py
@@ -2,22 +2,52 @@
 '''
 Created on 19 de Mar de 2013
 
-@author: admin1
+@author: António Baião N:5604
+@author: Carlos Palma N:5608
 '''
 
 import sys
 import re
 import pygeoip
-gi = pygeoip.GeoIP('../../../GeoIP.dat', pygeoip.MEMORY_CACHE)
-print gi.country_code_by_name('www.baidu.com')
-for line in sys.stdin:
-    if not re.search("SRC=192", line):
-        lista = line.split("SRC=")
-    IP = lista[1].split(' ' )[0]
-    try:
-        print IP + ': ' + gi.country_code_by_addr(IP)
-    except:
-        continue
+
+'''
+Classe de análise de ficheiro de logs
+'''
+class FicheiroLog:
+    def __init__(self):
+        caminho = '../../../GeoIP.dat'
+        self.analiseFicheiroLog(caminho)
+        pass
+    
+    '''
+    Método que faz a análise do Ficheiro de log
+    Recebe como parametro o caminho do file GeoIP.dat
+    '''
+    def analiseFicheiroLog(self, caminho):
+        gi = pygeoip.GeoIP(caminho, pygeoip.MEMORY_CACHE)
+        print gi.country_code_by_name('www.baidu.com')
+        for line in sys.stdin:
+            if not re.search("SRC=192", line):
+                lista = line.split("SRC=")
+                listaData = line.split(' ')
+            IP = lista[1].split(' ' )[0]
+            mes = listaData[0]
+            dia = listaData[1]
+            hora = listaData[2]
+            #print IP
+            try:
+                print IP + ': ' +  mes + ': '+ dia + ': ' + hora + ' --> ' + gi.country_code_by_addr(IP)
+            except:
+                continue
+        pass
+
+    pass
+
+
+if __name__ == '__main__':
+    FicheiroLog()    
+    
+
 
 #Makefile    
 '''
