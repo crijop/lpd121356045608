f99ed3e3af4cd1d50aa659482c3e1387370dc2f6
91195608 <91195608@alunos.ipbeja.pt>
2013-04-17 17:16:50 +0000

    file logs

--- src/teste.py
+++ src/teste.py
@@ -5,15 +5,17 @@ Created on 15 de Abr de 2013
 '''
 import os
 import pickle
-
+#oalaaaaaaaa
 if __name__ == '__main__':
     ano = 2002
     nrDocentes = [299, 90, 0, 5, 5]
-    
+    '''
     print nrDocentes
     ficheiro = open("mens.dat", "w")
     pickle.dump(ano, ficheiro)
     pickle.dump(nrDocentes, ficheiro)
     ficheiro.close()
     os.system("python Graficos.py")
+    
+    '''
     pass
